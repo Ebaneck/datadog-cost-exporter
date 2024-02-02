6aaf5e88a6fa2c527952f7df9a4428091b50beb3
Claude Ebaneck <claudeforlife@gmail.com>
2024-02-02 12:49:28 +0100

    ci: add semantic release with github actions

--- src/exporter.py
+++ src/exporter.py
@@ -9,9 +9,14 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from prometheus_client import Gauge
 from datadog_api_client import ApiClient, Configuration
-
+from datadog_api_client.v2.api.usage_metering_api import UsageMeteringApi
 
 class MetricExporter:
+    '''
+        Usage data is delayed by up to 72 hours from when it was incurred. 
+        It is retained for 15 months.
+        https://datadog-api-client.readthedocs.io/en/latest/datadog_api_client.v2.api.html#module-datadog_api_client.v2.api.usage_metering_api
+    '''
     def __init__(
         self,
         polling_interval_seconds,
@@ -33,14 +38,18 @@ class MetricExporter:
                 continue
             time.sleep(self.polling_interval_seconds)
 
-    def get_dd_account_session(self):
+    def get_api_instance(self):
         configuration = Configuration()
-        return
+        configuration.api_key["appKeyAuth"] = self.dd_app_key
+        configuration.api_key["apiKeyAuth"] = self.dd_api_key
+        with ApiClient(configuration=configuration) as api_client:
+            api_instance = UsageMeteringApi(api_client)
+            return api_instance
 
-    def query_dd_cost_endpoint(self, dd_client, group_by):
+    def query_dd_cost_endpoint(self, api_instance, group_by):
         return
 
     def fetch(self):
-        dd_client = self.get_dd_account_session()
-        response = self.query_dd_cost_endpoint(dd_client, group_by)
+        api_instance = self.get_api_instance()
+        response = self.query_dd_cost_endpoint(api_instance, group_by)
         return response
