e68ca6b217fd7f3bdc3500a0b0fae63774fd62b4
Claude Ebaneck <claudeforlife@gmail.com>
2024-02-02 16:41:36 +0100

    feat: add methods to add and define metrics (#11)

--- src/exporter.py
+++ src/exporter.py
@@ -7,9 +7,19 @@ import time
 import logging
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from prometheus_client import Gauge
+
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.usage_metering_api import UsageMeteringApi
+
+from prometheus_client import (
+    Gauge,
+    Counter,
+    Summary,
+    Histogram,
+    Info,
+)
+
+logger = logging.getLogger('datadog-cost-exporter')
 
 class MetricExporter:
     '''
@@ -28,6 +38,32 @@ class MetricExporter:
         self.dd_api_key = dd_api_key
         self.dd_app_key = dd_app_key
         self.dd_host = dd_host
+        self.default_labels = {}
+
+    def define_metrics(self):
+        """
+        Defines the Prometheus metrics object. 
+        Returns a dictionary of metrics.
+        """
+        metrics = {}
+
+        metrics["config_sample_metric"] = Gauge(
+            "sample_metric",
+            "This is a sample metric for the Datadog exporter.",
+            labels=tuple(self.default_labels.keys())
+        )
+        return metrics
+    
+    def add_metrics(self, metrics, value=None):
+        """
+        Adds the retrieved metrics to the pre-defined object.
+        metrics: dict
+            A dictionary of Prometheus metric objects.
+        """
+        metrics["config_sample_metric"].add_metric(
+            labels=tuple(self.default_labels.values()),
+            value=None
+        )
 
     def run_metrics_loop(self):
         while True:
@@ -46,10 +82,20 @@ class MetricExporter:
             api_instance = UsageMeteringApi(api_client)
             return api_instance
 
-    def query_dd_cost_endpoint(self, api_instance, group_by):
+    def query_dd_cost_endpoint(self, api_instance):
         return
 
     def fetch(self):
+        """
+        Fetch the metrics from the Datadog API and yield them.
+        """
+        logger.info("Collecting the metrics for a Prometheus client")
+        
         api_instance = self.get_api_instance()
-        response = self.query_dd_cost_endpoint(api_instance, group_by)
-        return response
+        response = self.query_dd_cost_endpoint(api_instance)
+        # restructure the response to be in a format that can be used by the prometheus client
+
+        metric_definitions = self.define_metrics()
+        self.add_metrics(metric_definitions, response)
+
+        yield from metric_definitions.values()
